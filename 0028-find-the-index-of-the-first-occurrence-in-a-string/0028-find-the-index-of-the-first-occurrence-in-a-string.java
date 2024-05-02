ca345e23f7dfe3db92cba69c0cd3a5e310e9b235
Alexandre Braxton Prisajni <122131079+sprisajni@users.noreply.github.com>
2024-05-02 07:48:14 -0700

    Time: 0 ms (100.00%), Space: 41.1 MB (89.51%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        for(int i = 0; i < haystack.length(); i++){
+            
+            if(haystack.startsWith(needle,i)){
+                
+                return i;
+            }
+            
+            
+            
+        }
+        
+        return -1;
+    }
+}
